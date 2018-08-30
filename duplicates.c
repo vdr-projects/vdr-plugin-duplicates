6ebc202bb2440f881b862b5b4ee26749a1a337fc
Timo Eskola <timo@tolleri.net>
2018-08-30 09:01:53 +0300

    Updated version

--- duplicates.c
+++ duplicates.c
@@ -12,7 +12,7 @@
 #include "menu.h"
 #include "visibility.h"
 
-static const char *VERSION        = "0.2.0";
+static const char *VERSION        = "0.2.1";
 static const char *DESCRIPTION    = trNOOP("Shows duplicate recordings");
 static const char *MAINMENUENTRY  = trNOOP("Duplicate recordings");
 
